676375e07dc7d8266f94371850fbb6c636c67033
Ezzywealth <geniuswealthb@gmail.com>
2022-02-21 13:37:47 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias
+        labore porro esse, ex amet pariatur consequuntur, laudantium dolorem
+        voluptate reprehenderit harum, neque repellat id sint asperiores fugit
+        tempora nulla nobis vel similique nemo quaerat in? Et nobis similique
+        aliquid laudantium, itaque aliquam velit dolorem dignissimos culpa fugit
+        asperiores vel nihil voluptatum harum, nisi sed odit ex consequatur at
+        atque! Quaerat aut distinctio vel itaque nesciunt ducimus ex nisi autem
+        voluptatibus magni. Velit deserunt cupiditate ut repudiandae sunt, natus
+        repellat fugiat ea reprehenderit commodi! Repudiandae quibusdam dolor
+        voluptatum, libero provident eos laborum cum quos, adipisci
+        necessitatibus cupiditate molestias sit odit suscipit?
+      </p>
+    </section>
+  );
+};
+
+export default About;
